dc89a200da764fbf8195dee7b158935b3cd60b89
andrey36101 <andreivanov86@gmail.com>
2017-04-13 21:33:53 +0100

    list user api added with filter and pagination

--- src/server/app.js
+++ src/server/app.js
@@ -9,33 +9,54 @@ const path         = require('path');
 const fs =require('fs');
 let index=fs.readFileSync(path.join(ROOT_PATH,'..','public','index.html'));
 //Configure application
+
+app.set('superSecret', config.secret);
 require('./config/mongo');
 require('./config/express')(app);
 
 //Configure api routes authentication
 app.use((req, res, next)=>{
-		if(req.path.indexOf('/apis') === 0){ // If request is starting with /apis, then apply authentication check
-			if(req.session.isLoggedIn === 'Y'){
-				next();
-			} else{
-				let secretKey    = req.headers['X-SECRET-KEY'];
-				let accessKey    = req.headers['X-ACCESS-KEY'];
-				let sessionToken = req.headers['X-SESSION-TOKEN'];
-				
-				// IMPLEMENT APP SPECIFIC AUTHENTICATION LOGIC
-				next();
-			}
-		} else{
-			next();
-		}
-	}
+        if(req.path.indexOf('/apis') === 0){ // If request is starting with /apis, then apply authentication check
+            if(req.session.isLoggedIn === 'Y'){
+                next();
+            } else{
+                let secretKey    = req.headers['X-SECRET-KEY'];
+                let accessKey    = req.headers['X-ACCESS-KEY'];
+                let sessionToken = req.headers['X-SESSION-TOKEN'];
+
+                // IMPLEMENT APP SPECIFIC AUTHENTICATION LOGIC
+                next();
+            }
+        } else{
+            next();
+        }
+    }
 );
+
+app.post('/uploadfiles', global.upload.array('file'), function(req, res){
+
+    let attachedFiles = [];
+    __lodash.forEach(req.files, function(value, key){
+        try{
+            attachedFiles.push({
+                'path':value.path,
+                'fileName':value.originalname,
+                'contentType':value.mimetype
+            });
+        } catch(err){
+            res.sendError(err);
+        }
+    });
+    console.log(attachedFiles);
+    res.sendResponse(attachedFiles);
+});
+
 app.all('/api', (req, res)=> {
-	res.send({"title":"Api Server running"});
+    res.send({"title":"Api Server running"});
 });
 let Api=require('./apis');
 new Api(app);
 app.all('/*', (req, res)=> {
-	res.sendError(new Exception('ObjectNotFound','Endpoint not found'));
+    res.sendError(new Exception('ObjectNotFound','Endpoint not found'));
 });
 module.exports = app;
--- src/server/models/sport.model.js
+++ src/server/models/sport.model.js
@@ -1,4 +1,4 @@
-module.exports = (mongoose) => {
+    module.exports = (mongoose) => {
     /**
      * Sports Schema
      */
